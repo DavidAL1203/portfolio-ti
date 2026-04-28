b94514197c52474c485e5260faae51af07a6f9a8
David Almir de Lima <davidalmir.lima1203@gmail.com>
2026-04-28 09:41:11 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,31 +1,38 @@
-def calculadora(op, num1, num2, resultado):
-    if op == 1:
-        resultado = num1 + num2
-    elif op == 2:
-        resultado = num1 - num2
-    elif op == 3:
-        resultado = num1 * num2
-    elif op == 4:
-        resultado = num1 / num2
-    print(f'Resultado: {resultado}')
-
-
+historico = []
 while True:
-    print(f'==========-CALCULADORA-==========')
-    opcao = (int(input('1 - SOMA \n'
-                          '2 - SUBTRAÇÃO \n'
-                          '3 - MULTIPLIÇÃO \n'
-                          '4 - DIVISÃO \n'
-                          '0 - SAIR \n'
-                          '================================= \n'
-                          'Escolha uma operação: ')))
-    if opcao != 0:
-        if opcao == 1 or opcao == 2 or opcao == 3 or opcao == 4:
-            n1 = (float(input(f'Digite o primeiro número: ')))
-            n2 = (float(input(f'Digite o segundo número: ')))
-            calculadora(opcao, n1, n2, 0)
-        else:
-            print('Opção Invalida.')
+  print(f'{'='*10}-CALCULADORA-{'='*10}')
+  op = int(input('1 - SOMA \n2 - SUBTRAÇÃO \n3 - MULTIPLICAÇÃO \n4 - DIVISÃO \n5 - HISTÓRICO \n0 - SAIR \nOPERAÇÃO: '))
+  if op == 0:
+    break
+  else:
+    if op == 1:
+      n1 = float(input('NÚMERO 1: '))
+      n2 = float(input('NÚMERO 2: '))
+      print(f'{n1} + {n2} = {n1+n2}')
+      historico.append(f'{n1} + {n2} = {n1+n2}\n')
+    elif op == 2:
+      n1 = float(input('NÚMERO 1: '))
+      n2 = float(input('NÚMERO 2: '))
+      print(f'{n1} - {n2} = {n1-n2}')
+      historico.append(f'{n1} - {n2} = {n1-n2}\n')
+    elif op == 3:
+      n1 = float(input('NÚMERO 1: '))
+      n2 = float(input('NÚMERO 2: '))
+      print(f'{n1} * {n2} = {n1*n2}')
+      historico.append(f'{n1} * {n2} = {n1*n2}\n')
+    elif op == 4:
+      n1 = float(input('NÚMERO 1: '))
+      n2 = float(input('NÚMERO 2: '))
+      if n2 == 0:
+        print('NÃO É POSSÍVEL DIVIDIR POR ZERO')
+      else:
+        print(f'{n1} / {n2} = {n1/n2}')
+        historico.append(f'{n1} / {n2} = {n1/n2}\n')
+    elif op == 5:
+      print('='*30)
+      for item in historico:
+        print(item)
     else:
-        break
+      print('OPÇÃO INVÁLIDA')
+    print('='*30)
 print('Saindo...')
